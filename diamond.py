605aa48905dbe256a10ca72d24b97afb14d95abe
Brady L. Hurlburt <bradyhurlburt@gmail.com>
2018-02-27 21:19:06 -0500

    Basic functionality

---
+++ diamond.py
@@ -0,0 +1,16 @@
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Build a diamond.')
+    parser.add_argument('-n', '--number', type=int, default=5, help='Input number [default 5]')
+    args = parser.parse_args()
+    n = args.number
+    print("Running diamond program with integer {}".format(n))
+    for row in list(range(n)):
+        if row == 0:
+            print('*' * (n - 1) + str(n) + '*' * (n-1)) 
+        else:
+            num_out = n - 1 - row
+            num_in = 2 * row -1
+            print('*' * num_out + str(n) + '*' * num_in + str(n) + '*' * num_out)
+
